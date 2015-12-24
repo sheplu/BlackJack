48d28fde82b85de5eaca5b46d08768d57a38c940
Sheplu <jean.burellier@gmail.com>
2015-12-24 10:20:24 +0100

    game menu

--- src/com/sheplu/blackjack/game/Game.java
+++ src/com/sheplu/blackjack/game/Game.java
@@ -1,5 +1,57 @@
 package com.sheplu.blackjack.game;
 
-public class Game {
+import java.util.Scanner;
 
+public class Game {
+	Scanner sc = new Scanner(System.in);
+	
+	public Game() {
+		System.out.println("initialize the game");
+		boolean isPlaying = true;
+		
+		do {
+			showMenu();
+			int choice = askChoice();
+			System.out.println("Hello! How many players today?");
+			
+			switch (choice) {
+				case 1:
+					System.out.println("play");
+					launcher();
+					break;
+				case 2:
+					System.out.println("no datas for now.");
+					break;
+				case 3:
+					System.out.println("quitting the game");
+					isPlaying = false;
+					break;
+				default:
+					System.out.println("no valid choice. try again");
+					break;
+			}
+			
+		}while(isPlaying);
+		
+		System.out.println("EOP");
+	}
+	
+	public void launcher() {
+		
+	}
+	
+	public void showMenu() {
+		System.out.println("Welcome !");
+		System.out.println("1 - Play");
+		System.out.println("2 - Datas");
+		System.out.println("3 - Quit");
+	}
+	
+	public int askChoice() {
+		int choice = sc.nextInt();
+		System.out.println(choice);
+		return choice;
+	}
+	
+	
 }
--- src/com/sheplu/blackjack/main/Main.java
+++ src/com/sheplu/blackjack/main/Main.java
@@ -1,10 +1,12 @@
 package com.sheplu.blackjack.main;
 
+import com.sheplu.blackjack.game.Game;
+
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Game game = new Game();
 	}
 
 }
